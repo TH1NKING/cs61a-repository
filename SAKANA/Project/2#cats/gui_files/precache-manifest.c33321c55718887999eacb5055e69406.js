908a992fa24121bbf5c99e5f17441ec2dd749cc1
LittleFish0403 <144256947+LittleFish0403@users.noreply.github.com>
2023-12-27 16:47:21 +0800

    Add files via upload

---
+++ SAKANA/Project/2#cats/gui_files/precache-manifest.c33321c55718887999eacb5055e69406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bc950a16a8b1d52c2901710baf5c19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "3191ff1083e5d7e338aa",
+    "url": "/static/css/main.dbfcd040.chunk.css"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/js/2.ecdaf578.chunk.js"
+  },
+  {
+    "revision": "3191ff1083e5d7e338aa",
+    "url": "/static/js/main.2479b07a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
